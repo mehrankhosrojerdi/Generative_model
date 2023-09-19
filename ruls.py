83d343461d0613ebcd0012846d88bb42e5bdfd95
mehrankhosrojerdi <mehrankhosrojerdi9@gmail.com>
2023-09-19 10:23:38 +0200

    new training set is added

---
+++ ruls.py
@@ -0,0 +1,26 @@
+coffee =  
+env =
+DARK =
+hrs =
+time = 
+BaseHuman = 
+"++" = vars
+
+
+
+
+
+class coder(BaseHuman):
+
+    def __init__(self):
+        coffee.strength ++                                                                                         
+        env.update()
+        env.them = DARK
+
+
+    def day(self):
+        self.eat(1*hrs)
+        self.code(12*hrs)
+        self.eat(1*hrs)
+        self.debug(4*hrs)
+        time.sleep(6*hrs)
